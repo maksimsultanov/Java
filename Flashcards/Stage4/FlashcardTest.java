7737d37ec5f3be27d7dbc2b5b8ec0968300d4482
maksimsultanov <maksimsultaniv@gmail.com>
2020-04-29 19:20:08 +0300

    FlashCard added test file

---
+++ Flashcards/Stage4/FlashcardTest.java
@@ -0,0 +1,82 @@
+package Stage4;
+
+import org.junit.Test;
+
+public class FlashcardTest {
+
+	@Test
+	public void testMain() throws Exception {
+
+	}
+
+	@Test
+	public void testCard() throws Exception {
+
+	}
+
+	@Test
+	public void testSetCardName() throws Exception {
+
+	}
+
+	@Test
+	public void testSetCardNumber() throws Exception {
+
+	}
+
+	@Test
+	public void testSetCardDifinition() throws Exception {
+
+	}
+
+	@Test
+	public void testGetCardName() throws Exception {
+
+	}
+
+	@Test
+	public void testGetCardDifinition() throws Exception {
+
+	}
+
+	@Test
+	public void testGetCardNumber() throws Exception {
+
+	}
+
+	@Test
+	public void testCard() throws Exception {
+
+	}
+
+	@Test
+	public void testSetCardName() throws Exception {
+
+	}
+
+	@Test
+	public void testSetCardNumber() throws Exception {
+
+	}
+
+	@Test
+	public void testSetCardDifinition() throws Exception {
+
+	}
+
+	@Test
+	public void testGetCardName() throws Exception {
+
+	}
+
+	@Test
+	public void testGetCardDifinition() throws Exception {
+
+	}
+
+	@Test
+	public void testGetCardNumber() throws Exception {
+
+	}
+
+}
